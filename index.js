0216a52f476a62c72e494d42753e2b6e92d46ae8
Will Munn <will.munn@focusrite.com>
2022-09-14 07:39:12 +0100

    Add help to cli interface

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 import {Encoder} from './lib/encoder.js';
 import {Decoder} from './lib/decoder.js';
 
+if (process.argv.includes('--help')) {
+  process.stdout.write(
+    `Usage:  base64 [--decode]
+  --help     display this message
+  --decode   decodes input`
+  );
+  process.exit(1);
+}
 if (process.argv.includes('--decode')) {
   process.stdin.pipe(new Decoder()).pipe(process.stdout);
 } else {
